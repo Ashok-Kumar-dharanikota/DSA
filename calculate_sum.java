2d859486aa9bae5dcdda722ef3c763a6b636ed28
Ashok Kumar Dharanikota <103619560+Ashok-Kumar-dharanikota@users.noreply.github.com>
2023-07-05 19:41:11 +0530

    Create calculate_sum.java

---
+++ calculate_sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+public class tuf {
+ 
+public static void main(String args[]){  
+   
+   int arr[] = {10,5,10,15,10,5};
+
+   int sum = 0;
+   for(int i=0; I<arr.length; i++) {
+      sum = arr[i] + sum; 
+   }
+
+   System.out.println(sum);
+   
+}
+}
