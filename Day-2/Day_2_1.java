3c38d31313d3b93ce64c0fbba99a87bd6217d1eb
Ashok-Kumar-dharanikota <ashok81432@gmail.com>
2023-09-11 11:38:41 +0530

    Day - 2 DSA-Arrays

---
+++ Day-2/Day_2_1.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Pair  
+{  
+    long first, second;  
+    public Pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class Day_2_1 {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            Pair product = obj.getMinMax(a, n); 
+            System.out.println(product.first+" "+product.second);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+ class Pair  
+{  
+    long first, second;  
+    public Pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+
+class Compute 
+{
+     Pair getMinMax(long a[], long n)  
+    {
+        //Write your code here
+        if(n==1) {
+            return new Pair(a[0], a[0]);
+        }
+        
+        long first = a[0], second = a[0];
+        
+        for(int i=1; i<n; i++) {
+            if(first > a[i]) {
+                first = a[i];
+            }
+            else if(second < a[i]) {
+                second = a[i];
+            }
+        }
+        
+        
+        return new Pair(first, second);
+
+    }
+}
